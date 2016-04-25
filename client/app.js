87b37fcebad389f3e39b0049d74bf25631e692ce
ansteh <stehle.andre@googlemail.com>
2016-04-25 13:35:00 +0200

    init

---
+++ client/app.js
@@ -0,0 +1,81 @@
+(function(){
+  var Graphics = {};
+  Graphics.npm = {};
+
+  Graphics.npm.downloads = function plot(anchor, info){
+    var data = info.downloads;
+    MG.convert.date(data, 'day', '%Y-%m-%d');
+    MG.data_graphic({
+      title: info.package,
+      //description: "dowloads of last month",
+      data: data,
+      full_width: true,
+      target: anchor,
+      x_accessor: 'day',
+      y_accessor: 'downloads'
+    });
+  };
+
+  var app = angular.module('app', ['ngMaterial']);
+
+  app.factory('Npm', function($http){
+    function downloads(name){
+      var url = 'https://api.npmjs.org/downloads/range/last-month/'+encodeURIComponent(name);
+      return new Promise(function(resolve, reject){
+        $http({
+          method: 'GET',
+          url: url
+        }).then(function (response) {
+            resolve(response.data);
+          }, function (err) {
+            reject(err);
+          });
+      });
+    };
+
+    return {
+      downloads: downloads,
+    };
+  });
+
+  app.directive('userNpmModules', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'client/user-npm-modules.tpl.html',
+      scope: {},
+      controller: function($scope){
+        $scope.modules = [
+          'shape-json',
+          'shape-array',
+          'difference-json',
+          'brain-pact',
+          'datamuse',
+          'mock-realtime',
+          'url-graph',
+          'repute'
+        ];
+      }
+    };
+  });
+
+  app.directive('npmModule', function(Npm){
+    return {
+      restrict: 'E',
+      templateUrl: 'client/npm-module.tpl.html',
+      scope: { name: "="},
+      controller: function($scope, $element){
+        var anchor = angular.element($element).find('#downloads')[0];
+        console.log($scope.name);
+
+        Npm.downloads($scope.name)
+        .then(function(downloads){
+          console.log(downloads);
+          Graphics.npm.downloads(anchor, downloads);
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+      }
+    };
+  });
+}());
